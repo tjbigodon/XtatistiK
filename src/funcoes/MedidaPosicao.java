32f2a8c1108a72433c0d79888d89a679205f6d4d
livre <livre@localhost>
2017-03-15 16:10:21 -0300

    Criação de Funções

---
+++ src/funcoes/MedidaPosicao.java
@@ -0,0 +1,13 @@
+/**
+ * Classe responsável pela execução das funções aritmeticas relativas a
+ * dados estatísticos.
+ */
+package funcoes;
+
+/**
+ * 
+ * @author Jehymison Gil Alves Oliveira
+ */
+public class MedidaPosicao {
+    
+}
