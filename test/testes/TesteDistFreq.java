29664deb3c1d727748235c8d1734772591409623
Tarcisio <Tarcisio@Asus-2377M>
2017-03-26 21:13:18 -0300

    No pacote de testes, construção da tabela de frequÊncias concluída. (Código comentado)

--- test/testes/TesteDistFreq.java
+++ test/testes/TesteDistFreq.java
@@ -14,9 +14,9 @@ import java.util.ArrayList;
  */
 public class TesteDistFreq {
     public static void main(String[] args) {
-        ArrayList<Double> valores = new ArrayList();
-        int numClasses = 0;
-        double amplitudeClasses = 0;
+        ArrayList<Double> valores = new ArrayList(); //arraylist com todos os valores da amostra
+        int numClasses = 0; //conterá o número de classes a aparecer na tabela
+        double amplitudeClasses = 0; //conterá o valor do cálculo da amplitude de classes
         
         valores.add(new Double(89));
         valores.add(new Double(85));
@@ -34,18 +34,19 @@ public class TesteDistFreq {
         valores.add(new Double(84));
         valores.add(new Double(82));
         
-        
+        //condicional que calcula o número de classes
         if(valores.size()<=25){
             numClasses = 5;
         }else{
             numClasses = (int) Math.sqrt(valores.size());
         }
         
-        int []freqAbs = new int[numClasses];
+        int []freqAbs = new int[numClasses];//guardará as frequências absolutas.
         
-        double maior = valores.get(0);
-        double menor = valores.get(0);
+        double maior = valores.get(0);//maior valor da amostra
+        double menor = valores.get(0);//menor valor da amostra
         
+        //laço que verifica qual o maior e menor valores da amostra
         for(int i = 0; i<valores.size(); i++){
             if(valores.get(i)>maior){
                 maior = valores.get(i);
@@ -54,20 +55,23 @@ public class TesteDistFreq {
             }
         }
         
-        amplitudeClasses = ((maior - menor)/numClasses)*1.1;
         
-        double []valoresIniciais = new double[numClasses];
-        double []valoresFinais = new double[numClasses];
+        amplitudeClasses = ((maior - menor)/numClasses)*1.1; //cálculo da amplitude
         
-        valoresIniciais[0] = formata(menor);
+        
+        double []valoresIniciais = new double[numClasses];//conterá os valores iniciais das classes
+        double []valoresFinais = new double[numClasses];//conterá os valores finais das classes
+        
+        valoresIniciais[0] = formata(menor); //valor inicial da primeira classe é o menor valor do grupo
         valoresFinais[0] = formata(menor + amplitudeClasses);
         
-        
+        //laço que preenche os valores da tabela
         for(int i = 1; i<freqAbs.length; i++){
             valoresIniciais[i] = valoresFinais[i-1];
             valoresFinais[i] = formata(valoresIniciais[i] + amplitudeClasses);
         }
         
+        //calcula a frequência absoluta para colocar na tabela
         for(int i = 0; i<valoresIniciais.length; i++){
             for(int j = 0; j<valores.size(); j++){
                 if(valores.get(j)>=valoresIniciais[i]&&valores.get(j)<valoresFinais[i]){
@@ -76,6 +80,7 @@ public class TesteDistFreq {
             }
         }
         
+        //imprime a tabela formatada
         for(int i = 0; i<valoresIniciais.length; i++){
             System.out.print(valoresIniciais[i]+"|-----"+valoresFinais[i]);
             System.out.print("      "+freqAbs[i]);
@@ -83,8 +88,9 @@ public class TesteDistFreq {
         }
     }
     
+    //formata um double com duas casas decimais
     public static double formata(double item){
-        String aux = String.format("%.2f", item);
+        String aux = String.format("%.2f", item); //"String.format" retorna um float, então, a função retorna um double.
         char []arrayChar = new char[aux.length()];
         
         arrayChar = aux.toCharArray();
